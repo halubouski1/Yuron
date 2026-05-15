9cdf79d14da943f417b2a4483444ec4a1b671f5e
halubouski1 <halubouski2@gmail.com>
2026-05-15 23:07:05 +0300

    removed swiper states

--- js/main.js
+++ js/main.js
@@ -138,13 +138,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Before & After slider
     if (document.querySelector(".before-after__slider")) {
-      const isMobileBA = () => window.innerWidth <= 560;
-
-      const baSwiper = new Swiper(".before-after__slider", {
+      new Swiper(".before-after__slider", {
         slidesPerView: "auto",
         spaceBetween: 20,
         grabCursor: true,
-        noSwiping: !isMobileBA(),
+        noSwiping: true,
         noSwipingSelector: "img-comparison-slider",
         breakpoints: {
           0: { spaceBetween: 6 },
@@ -159,11 +157,6 @@ document.addEventListener("DOMContentLoaded", () => {
           type: "bullets",
           clickable: true,
         },
-      });
-
-      window.addEventListener("resize", () => {
-        baSwiper.params.noSwiping = !isMobileBA();
-        baSwiper.update();
       });
     }
 
